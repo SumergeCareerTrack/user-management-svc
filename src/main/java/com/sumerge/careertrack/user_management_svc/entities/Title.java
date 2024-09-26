621506df6dceef77b47bbea9f46e16507291a276
Youssef Mahmoud <ymahmoud@sumerge.com>
2024-09-26 14:36:51 +0300

    Added NoArgsContructors

--- src/main/java/com/sumerge/careertrack/user_management_svc/entities/Title.java
+++ src/main/java/com/sumerge/careertrack/user_management_svc/entities/Title.java
@@ -6,10 +6,12 @@ import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Title {
     @EmbeddedId
     private TitleId id;
--- src/main/java/com/sumerge/careertrack/user_management_svc/entities/compositeKeys/TitleId.java
+++ src/main/java/com/sumerge/careertrack/user_management_svc/entities/compositeKeys/TitleId.java
@@ -5,10 +5,12 @@ import java.io.Serializable;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Embeddable
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TitleId implements Serializable {
     private String department;
     private String name;
