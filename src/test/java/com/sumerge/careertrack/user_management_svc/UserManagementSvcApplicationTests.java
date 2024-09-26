300ebdee8e9c8dae3cddeca15fe98f8a86657f74
Youssef Mahmoud <ymahmoud@sumerge.com>
2024-09-26 12:04:46 +0300

    Initial Commit

---
+++ src/test/java/com/sumerge/careertrack/user_management_svc/UserManagementSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumerge.careertrack.user_management_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
