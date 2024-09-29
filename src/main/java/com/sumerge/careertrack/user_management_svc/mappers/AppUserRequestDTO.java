ba01b8e38935770e0e99215ede319d99e82b49b9
Youssef Fahmy <youssef.tfs2@gmail.com>
2024-09-29 12:41:03 +0300

    Added @Builder to classes

--- src/main/java/com/sumerge/careertrack/user_management_svc/mappers/AppUserRequestDTO.java
+++ src/main/java/com/sumerge/careertrack/user_management_svc/mappers/AppUserRequestDTO.java
@@ -3,9 +3,11 @@ package com.sumerge.careertrack.user_management_svc.mappers;
 import java.util.UUID;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 @AllArgsConstructor
 public class AppUserRequestDTO {
 
