ad317b215bed3ea1623832a40efccea74e556db5
Youssef Fahmy <youssef.tfs2@gmail.com>
2024-09-29 14:35:58 +0300

    Fixed Exceptions

--- src/main/java/com/sumerge/careertrack/user_management_svc/exceptions/DoesNotExistException.java
+++ src/main/java/com/sumerge/careertrack/user_management_svc/exceptions/DoesNotExistException.java
@@ -4,6 +4,7 @@ public class DoesNotExistException extends UserManagementException {
 
     public static final String APP_USER_ID = "User with ID \"%d\" does not exist.";
     public static final String APP_USER_EMAIL = "User with email \"%d\" does not exist.";
+    public static final String DEPARTMENT = "Department \"%s\" does not exist.";
     public static final String TITLE = "Title \"%s\" in department \"%s\" does not exist.";
 
     public DoesNotExistException() {
--- src/main/java/com/sumerge/careertrack/user_management_svc/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/sumerge/careertrack/user_management_svc/exceptions/GlobalExceptionHandler.java
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler({ DoesNotExistException.class })
-    public ResponseEntity<Object> handleCourseAlreadyExistsException(DoesNotExistException exception) {
+    public ResponseEntity<Object> handleDoesNotExistException(DoesNotExistException exception) {
         return ResponseEntity.badRequest()
                 .body(exception.getMessage());
     }
 
     @ExceptionHandler({ AlreadyExistsException.class })
-    public ResponseEntity<Object> handleCourseAlreadyExistsException(AlreadyExistsException exception) {
+    public ResponseEntity<Object> handleAlreadyExistsException(AlreadyExistsException exception) {
         return ResponseEntity.badRequest()
                 .body(exception.getMessage());
     }
