5d0274d02d6eb42f1dffc652f7ae8c2aedd50674
Youssef Fahmy <youssef.tfs2@gmail.com>
2024-09-30 17:28:02 +0300

    Allowed error messages to pass through

--- src/main/java/com/sumerge/careertrack/user_management_svc/configs/SecurityConfig.java
+++ src/main/java/com/sumerge/careertrack/user_management_svc/configs/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.authentication.session.SessionFixationPr
 
 import com.sumerge.careertrack.user_management_svc.filters.JwtAuthenticationFilter;
 
+import jakarta.servlet.DispatcherType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -35,14 +36,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/auth/**").permitAll()
-                .anyRequest().authenticated())
-            .sessionManagement(session -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authenticationProvider(authenticationProvider)
-            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-            .userDetailsService(userDetailsService);
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .userDetailsService(userDetailsService);
 
         return http.build();
     }
