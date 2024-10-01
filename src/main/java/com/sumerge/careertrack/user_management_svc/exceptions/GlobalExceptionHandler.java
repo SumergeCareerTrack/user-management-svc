c7448f9c3582ecd6a83313601782781a9ec32a63
Youssef Fahmy <youssef.tfs2@gmail.com>
2024-10-01 11:09:34 +0300

    Created Invalid Credentials Exception

--- src/main/java/com/sumerge/careertrack/user_management_svc/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/sumerge/careertrack/user_management_svc/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.sumerge.careertrack.user_management_svc.exceptions;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,14 +10,17 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler({ DoesNotExistException.class })
     public ResponseEntity<Object> handleDoesNotExistException(DoesNotExistException exception) {
-        return ResponseEntity.badRequest()
-                .body(exception.getMessage());
+        return ResponseEntity.badRequest().body(exception.getMessage());
     }
 
     @ExceptionHandler({ AlreadyExistsException.class })
     public ResponseEntity<Object> handleAlreadyExistsException(AlreadyExistsException exception) {
-        return ResponseEntity.badRequest()
-                .body(exception.getMessage());
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    @ExceptionHandler({ InvalidCredentialsException.class })
+    public ResponseEntity<Object> handleInvalidCredentialsException(InvalidCredentialsException exception) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(exception.getMessage());
     }
 
 }
--- src/main/java/com/sumerge/careertrack/user_management_svc/service/AuthService.java
+++ src/main/java/com/sumerge/careertrack/user_management_svc/service/AuthService.java
@@ -16,6 +16,7 @@ import com.sumerge.careertrack.user_management_svc.entities.requests.RegisterReq
 import com.sumerge.careertrack.user_management_svc.entities.responses.AuthenticationResponse;
 import com.sumerge.careertrack.user_management_svc.exceptions.AlreadyExistsException;
 import com.sumerge.careertrack.user_management_svc.exceptions.DoesNotExistException;
+import com.sumerge.careertrack.user_management_svc.exceptions.InvalidCredentialsException;
 import com.sumerge.careertrack.user_management_svc.repositories.AppUserRepository;
 import com.sumerge.careertrack.user_management_svc.repositories.DepartmentRepository;
 import com.sumerge.careertrack.user_management_svc.repositories.TitleRepository;
@@ -26,68 +27,68 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthService {
 
-    private final AppUserRepository appUserRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final JwtService jwtService;
-    private final AuthenticationManager authenticationManager;
-    private final TitleRepository titleRepository;
-    private final DepartmentRepository deptRepository;
+	private final AppUserRepository appUserRepository;
+	private final PasswordEncoder passwordEncoder;
+	private final JwtService jwtService;
+	private final AuthenticationManager authenticationManager;
+	private final TitleRepository titleRepository;
+	private final DepartmentRepository deptRepository;
 
-    public AuthenticationResponse register(RegisterRequest request) {
-        if (appUserRepository.existsByEmail(request.getEmail())) {
-            throw new AlreadyExistsException(
-                    AlreadyExistsException.APP_USER_EMAIL, request.getEmail());
-        }
+	public AuthenticationResponse register(RegisterRequest request) {
+		if (appUserRepository.existsByEmail(request.getEmail())) {
+			throw new AlreadyExistsException(
+					AlreadyExistsException.APP_USER_EMAIL, request.getEmail());
+		}
 
-        Department userDept = deptRepository.findById(request.getDepartment())
-                .orElseThrow(() -> new DoesNotExistException(
-                        DoesNotExistException.DEPARTMENT, request.getDepartment()));
+		Department userDept = deptRepository.findById(request.getDepartment())
+				.orElseThrow(() -> new DoesNotExistException(
+						DoesNotExistException.DEPARTMENT, request.getDepartment()));
 
-        Title userTitle = titleRepository.findById(request.getTitle())
-                .orElseThrow(() -> new DoesNotExistException(
-                        DoesNotExistException.TITLE, request.getTitle(), userDept.getName()));
+		Title userTitle = titleRepository.findById(request.getTitle())
+				.orElseThrow(() -> new DoesNotExistException(
+						DoesNotExistException.TITLE, request.getTitle(), userDept.getName()));
 
-        AppUser newUser = AppUser.builder()
-                .email(request.getEmail())
-                .password(passwordEncoder.encode(request.getPassword()))
-                .firstName(request.getFirstName())
-                .lastName(request.getLastName())
-                .title(userTitle)
-                .department(titleRepository.findById(request.getTitle()).get().getDepartment())
-                .build();
+		AppUser newUser = AppUser.builder()
+				.email(request.getEmail())
+				.password(passwordEncoder.encode(request.getPassword()))
+				.firstName(request.getFirstName())
+				.lastName(request.getLastName())
+				.title(userTitle)
+				.department(titleRepository.findById(request.getTitle()).get().getDepartment())
+				.build();
 
-        if (request.getManagerId() != null) {
-            UUID managerId = request.getManagerId();
-            AppUser manager = appUserRepository.findById(managerId)
-                    .orElseThrow(() -> new DoesNotExistException(
-                            DoesNotExistException.APP_USER_ID, managerId));
+		if (request.getManagerId() != null) {
+			UUID managerId = request.getManagerId();
+			AppUser manager = appUserRepository.findById(managerId)
+					.orElseThrow(() -> new DoesNotExistException(
+							DoesNotExistException.APP_USER_ID, managerId));
 
-            newUser.setManager(manager);
-        }
+			newUser.setManager(manager);
+		}
 
-        appUserRepository.save(newUser);
-        String jwtToken = jwtService.generateToken(newUser);
-        jwtService.saveTokenInRedis(request.getEmail(), jwtToken);
-        return new AuthenticationResponse(jwtToken);
-    }
+		appUserRepository.save(newUser);
+		String jwtToken = jwtService.generateToken(newUser);
+		jwtService.saveTokenInRedis(request.getEmail(), jwtToken);
+		return new AuthenticationResponse(jwtToken);
+	}
 
-    // TODO: Review Exception
-    public AuthenticationResponse login(AuthenticationRequest request) {
-        try {
-            authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(
-                            request.getEmail(),
-                            request.getPassword()));
-        } catch (AuthenticationException e) {
-            throw new RuntimeException("Invalid email or password");
-        }
+	// TODO: Review Exception
+	public AuthenticationResponse login(AuthenticationRequest request) {
+		try {
+			authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(
+							request.getEmail(),
+							request.getPassword()));
+		} catch (AuthenticationException e) {
+			throw new InvalidCredentialsException();
+		}
 
-        AppUser user = appUserRepository.findByEmail(request.getEmail())
-                .orElseThrow(() -> new DoesNotExistException(
-                        DoesNotExistException.APP_USER_EMAIL, request.getEmail()));
-        String jwtToken = jwtService.generateToken(user);
-        jwtService.saveTokenInRedis(request.getEmail(), jwtToken);
-        return new AuthenticationResponse(jwtToken);
-    }
+		AppUser user = appUserRepository.findByEmail(request.getEmail())
+				.orElseThrow(() -> new DoesNotExistException(
+						DoesNotExistException.APP_USER_EMAIL, request.getEmail()));
+		String jwtToken = jwtService.generateToken(user);
+		jwtService.saveTokenInRedis(request.getEmail(), jwtToken);
+		return new AuthenticationResponse(jwtToken);
+	}
 
 }
