992157a0098b00e9017c8a4290b1778721be3386
Youssef Fahmy <youssef.tfs2@gmail.com>
2024-10-10 13:09:22 +0300

    Quickfix

--- src/main/java/com/sumerge/careertrack/user_management_svc/filters/JwtAuthenticationFilter.java
+++ src/main/java/com/sumerge/careertrack/user_management_svc/filters/JwtAuthenticationFilter.java
@@ -12,7 +12,7 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.sumerge.careertrack.user_management_svc.service.JwtService;
+import com.sumerge.careertrack.user_management_svc.services.JwtService;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -20,47 +20,46 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
-
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
-    //TODO: try to make the userDetailsService Final and remove the @Autowired
+    // TODO: try to make the userDetailsService Final and remove the @Autowired
     @Autowired
     private UserDetailsService userDetailsService;
+
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,
-     @NonNull HttpServletResponse response,
-     @NonNull FilterChain filterChain)
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain)
             throws ServletException, IOException {
-                final String authorizationHeader = request.getHeader("Authorization");
-                final String jwt;
-                final String userEmail;
-                if (request.getRequestURI().equals("/auth/login")||authorizationHeader == null ||  ( !authorizationHeader.startsWith("Bearer "))) {
-                    filterChain.doFilter(request, response);
-                    
-                    return;
-                }
-                jwt = authorizationHeader.substring(7);
-                userEmail = jwtService.extractUserEmail(jwt);
-                if(userEmail!=null && SecurityContextHolder.getContext().getAuthentication() == null) {
-                    UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
-                    try{
-                        if(jwtService.isTokenValid(jwt, userDetails)) {
-                            UsernamePasswordAuthenticationToken authToken =
-                                    new UsernamePasswordAuthenticationToken(
-                                            userDetails,
-                                            null,
-                                            userDetails.getAuthorities());
-                            authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                            SecurityContextHolder.getContext().setAuthentication(authToken);
-                        }
-                    }
-                    catch(Exception e){
-                        SecurityContextHolder.clearContext();
-                    }
-                }
-                filterChain.doFilter(request, response);
+        final String authorizationHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        if (request.getRequestURI().equals("/auth/login") || authorizationHeader == null
+                || (!authorizationHeader.startsWith("Bearer "))) {
+            filterChain.doFilter(request, response);
 
+            return;
+        }
+        jwt = authorizationHeader.substring(7);
+        userEmail = jwtService.extractUserEmail(jwt);
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            try {
+                if (jwtService.isTokenValid(jwt, userDetails)) {
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                            userDetails,
+                            null,
+                            userDetails.getAuthorities());
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            } catch (Exception e) {
+                SecurityContextHolder.clearContext();
             }
         }
+        filterChain.doFilter(request, response);
+
+    }
+}
